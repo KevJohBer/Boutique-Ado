199f9762b877a94de7191cb9067958cf808609ad
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-04-12 10:07:30 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,7 +15,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Mw012EouvGFrWtQEn0OXQ4pbpQ5jHiAWSK08675aRTGMwhatjzzXh6POQEOQHDGywCzjH8uLULdbLJvtODdgyPA00UYrxakiK',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
